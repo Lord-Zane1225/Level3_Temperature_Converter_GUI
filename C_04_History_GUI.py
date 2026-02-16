4b91181bebedbff3f442439e356ae067da03d20d
Lord_Zane021 <140982529+Lord-Zane1225@users.noreply.github.com>
2026-02-17 10:50:48 +1300

    History / Export with placeholder

--- C_04_History_GUI.py
+++ C_04_History_GUI.py
@@ -42,7 +42,7 @@ class HistoryExport:
         self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history, partner))
 
         # frame
-        self.history_frame = Frame(self.history_box, width=300, height=200)
+        self.history_frame = Frame(self.history_box)
         self.history_frame.grid()
 
         # strings for 'long' labels
@@ -67,7 +67,8 @@ class HistoryExport:
             make_label = Label(self.history_box, text=item[0], font=item[1], bg=item[2], wraplength=300, justify="left", pady=10, padx=20)
             make_label.grid(row=count)
 
-        history_label_ref.append(make_label)
+            history_label_ref.append(make_label)
+
 
         # configure it to show the filename if the user exports the file
         self.export_filename_label = history_label_ref[3]
@@ -86,7 +87,7 @@ class HistoryExport:
 
         for btn in button_details_list:
             self.make_button = Button(self.hist_button_frame, font=("Arial", 12, "bold"), text=btn[0], bg=btn[1], fg="#FFFFFF", width=12, command=btn[2])
-            self.make_button.grid(row=btn[3], columnspan=btn[4], padx=10, pady=10)
+            self.make_button.grid(row=btn[3], column=btn[4], padx=10, pady=10)
 
 
     def close_history(self, partner):
